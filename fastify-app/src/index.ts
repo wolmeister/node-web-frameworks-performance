a347bf4a5be03aee9548f39dd3207f5257002d3a
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-07-17 14:35:06 -0300

    Setup fastify app

--- fastify-app/src/index.ts
+++ fastify-app/src/index.ts
@@ -1,1 +1,18 @@
-console.log('fastify-app');
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.get('/health', (req, res) => {
+  res.send({
+    message: 'OK',
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.listen(3000, (error, address) => {
+  if (error) {
+    throw error;
+  }
+  console.log('Fastify listening on ' + address);
+});
