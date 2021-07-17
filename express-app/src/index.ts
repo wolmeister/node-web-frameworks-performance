62faa871f6157924bc12a38aee4b93fe665253d4
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-07-17 14:34:52 -0300

    Setup express app

--- express-app/src/index.ts
+++ express-app/src/index.ts
@@ -1,1 +1,15 @@
-console.log('express-app');
+import express from 'express';
+
+const app = express();
+
+app.get('/health', (req, res) => {
+  res.send({
+    message: 'OK',
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Express listening on http://localhost:3000');
+});
