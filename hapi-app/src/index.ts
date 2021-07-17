eec4a1c1302db131666e7f57230ae6001375ef7c
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-07-17 14:57:46 -0300

    Setup hapi project

--- hapi-app/src/index.ts
+++ hapi-app/src/index.ts
@@ -1,1 +1,30 @@
-console.log('hapi-app');
+import Hapi from '@hapi/hapi';
+
+const start = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/health',
+    handler: () => {
+      return {
+        message: 'OK',
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString(),
+      };
+    },
+  });
+
+  await server.start();
+  console.log('Server running on ' + server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(-1);
+});
+
+start();
