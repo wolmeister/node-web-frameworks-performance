1ce7a15dd451e2af1ff55d7cc8f19d446471196e
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-08-02 19:35:21 -0300

    Add prometheus to express

--- express-app/src/index.ts
+++ express-app/src/index.ts
@@ -1,8 +1,10 @@
 import express, { ErrorRequestHandler } from 'express';
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 import { ValidationError } from 'joi';
+import prometheus from 'prom-client';
 import { HttpError, runMigrations } from '@node-web-frameworks-performance/shared';
 
+import { handle } from './common/handle-async-middleware';
 import { authRouter } from './modules/auth';
 import { productRouter } from './modules/product';
 import { userRouter } from './modules/user';
@@ -15,6 +17,35 @@ const start = async () => {
   const app = express();
   app.use(express.json());
 
+  // Setup prometheus
+  const prometheusRegister = new prometheus.Registry();
+  prometheus.collectDefaultMetrics({ register: prometheusRegister });
+
+  const httpRequestTimer = new prometheus.Histogram({
+    name: 'http_request_duration_seconds',
+    help: 'Duration of HTTP requests in seconds',
+    labelNames: ['method', 'path', 'code'],
+    buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10],
+  });
+  prometheusRegister.registerMetric(httpRequestTimer);
+
+  app.get(
+    '/metrics',
+    handle(async (req, res) => {
+      res.header('Content-Type', prometheusRegister.contentType);
+      res.send(await prometheusRegister.metrics());
+    })
+  );
+
+  app.use((req, res, next) => {
+    const endTimer = httpRequestTimer.startTimer();
+    res.on('finish', () => {
+      endTimer({ path: req.path, code: res.statusCode, method: req.method });
+    });
+    next();
+  });
+
+  // Setup routes
   app.get('/health', (req, res) => {
     res.json({
       message: 'OK',
