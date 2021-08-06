83206936a1d9b3456fd2bd9ccd07b3dc39904c0d
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-08-06 20:44:24 -0300

    Change migrations to create db

--- shared/src/run-migrations.ts
+++ shared/src/run-migrations.ts
@@ -1,8 +1,18 @@
 import { join } from 'path';
 import { migrate } from 'postgres-migrations';
 
-import { dbPool } from './db';
+import { config } from './config';
 
 export async function runMigrations() {
-  await migrate({ client: dbPool }, join(__dirname, '..', 'migrations'));
+  await migrate(
+    {
+      database: config.get('db.database'),
+      user: config.get('db.user'),
+      password: config.get('db.password'),
+      host: config.get('db.host'),
+      port: config.get('db.port'),
+      ensureDatabaseExists: true,
+    },
+    join(__dirname, '..', 'migrations')
+  );
 }
